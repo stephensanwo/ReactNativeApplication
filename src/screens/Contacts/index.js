a8254f07c684c41728e2a313a3fbc93d3ccfceec
stephen sanwo <me@stephensanwo.com>
2021-03-03 18:49:22 +0100

    Screens

---
+++ src/screens/Contacts/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+const Contacts = () => {
+  return (
+    <View>
+      <Text> Hi from Contact</Text>
+    </View>
+  );
+};
+
+export default Contacts;
